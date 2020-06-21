6eaba03f1e36e716210bf7e5a7cc1651017229fa
GradyMellin <mellingrady@gmail.com>
2020-06-20 22:11:30 -0600

    Django University

---
+++ DjangoUniversity/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Grady Mellin\Desktop\Python_Course\Large_Python_Projs\DjangoUniversity\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
